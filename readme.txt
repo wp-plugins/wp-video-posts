c29062da3a7170098122f5c8766aacf811d8b095
cmstactics <cmstactics@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-14 22:25:45 +0000

    Adding previous versions to the repository
    git-svn-id: https://plugins.svn.wordpress.org/wp-video-posts/tags/1.2@625419 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,114 @@
+=== WP Video Posts ===
+Contributors: AlexRayan, cmstactics
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=J535UTFPCXFQC
+Tags: video converter, video plugin, ffmpeg, video post
+Requires at least: 3.2.1
+Tested up to: 3.4
+Stable tag: 1.2
+
+Upload videos to create custom video posts. With FFMPEG installed, it encodes
+and creates splash image. Supports FLV, F4V, MP4, AVI, MOV, 3GP and WMV formats.
+
+== Description ==
+
+WP Video Posts will enable you to create custom Video posts, upload and insert
+videos into these posts. All uploaded video files will be converted into MP4
+format to enhance the performance so that it loads and plays fast.  It creates
+snapshot of a specified frame at a specific time to create the splash image
+for the video. This plugin enables WordPress the ability to allow the
+following formats to be uploaded as well as convert them to the finalized MP4
+format for playing; FLV, F4V, MP4, AVI, MOV, 3GP and WMV formats.
+
+In addition, WP Video Posts allows the embed of Youtube and Vimeo videos with
+the use of the following shortcodes:
+
+Youtube:
+[wpvp_embed type=youtube video_code=vAFQIciWsF4 width=560 height=315]
+
+Vimeo:
+[wpvp_embed type=vimeo video_code=23117398 width=500 height=281]
+
+In order for all WP Video Posts features to work properly, then FFMPEG is required and need to be installed on your server.
+
+- Server should support **ffmpeg**
+- PHP needs to be compiled with **ffmpeg-PHP extension**
+
+If FFMPEG is not installed, then the plugin still allows you to upload videos to create custom posts, but the video is not encoded nor is a splash image created.  The default image will show as the splash image.
+
+= Instructions =
+1. After install, go to the Dashboard.
+
+2. Hover over the Videos menu item and click on Add New Video in the submen for Videos.
+
+3. Add a title to the Video Post.
+
+4. Click the Upload/Insert icon above the post content editor.
+
+5. In the media uploader pop up, add the video you want to attach to this video post.
+
+6. After the video uploads, it will automatically be encoded.  After this process, the video attachment details open where you can modify the title, caption and description.
+
+7. Once the details have been modified/added if you chose to add those details, click on the Insert into Post button.  It will then add a shortcode that will appear as the following: [wpvp_flowplayer src=http://yoursite.com/wp-content/uploads/2012/06/MyCar.mp4 width=640 height=360 splash=http://yoursite.com/wp-content/uploads/2012/06/MyCar.jpg] 
+
+8. Add any other details in the post content and click the Publish button.
+
+9. That is all.
+
+== Installation ==
+
+- Upload the wp_video_posts.zip file into `/wp-content/plugins/` directory
+- Activate the plugin through the 'Plugins' menu in WordPress
+- Under 'Settings'->'WP Video Posts' customize options for width and height
+  of a video and a thumb, as well as the frame (in seconds) the thumb should be generated from
+- Under 'Settings'->'Permalinks' click Save to refresh your permalink
+  structure.
+
+== Frequently Asked Questions ==
+
+Q: I get a "Page not found" error when I view my new video post. Why is this
+happening? 
+A: With any new custom post type being registered with WordPress, the permalinks need to be updated.  The solution is go to the 'Settings'->'Permalinks' and save your current links structure again. 
+
+Q: I'm running WordPress multisite and I get the message that says something about the file type not being supported.  How do I fix that?
+A: If you are using WordPress multisite, then you need to manually list the type of video formats to allow for upload.  This is done by logging in to the wp-admin, and going to 'My Site' => 'Network Admin', then click on 'Settings' => 'Network Settings'.
+Scroll down to the Upload Settings section of the network settings page and add the format in the Upload file types list.
+
+Q: I'm using the plugin but I do not have FFMPEG installed on my server.  How can I create my own default splash image and have it display rather than the default image supplied with the plugin?
+A: You can create an image with the dimensions you want and upload it to your server to override our default_image.png located in the /wp-content/plugins/wp-video-posts/images/ directory.
+
+= What pre-requirements do I need to install this plugin? =
+
+You must Install ffmpeg on your server and recompile PHP with ffmpeg-PHP.  
+
+= What happen if I dont have ffmpeg in my server? =
+
+If you do not have ffmpeg support on your server, this plugin will simply ignore the conversion and proceed with the rest of the process.  The supported file formats without ffmpeg installed would only be FLV and MP4.  In addition, the splash image will not be generated either and the default image will be displayed.
+
+== Screenshots ==
+
+1. Video Posts page displaying your video posts.
+
+2. WP Video Posts, edit post page.
+
+3. WP Video Posts Options page.
+
+4. WP Video Posts Widet Options.
+
+5. WP Video Posts Widget in action.
+
+== Changelog ==
+= 1.2 =
+- Added a widget to the plugin to allow you to display your videos that were uploaded or from YouTube or Vimeo.
+- The added widget can display your videos in a vertical or horizontal layout.  It also gives you the options to show the player or thumbnails that link to the video post.  See screenshot-4 for more options available in the widget.
+
+= 1.1 =
+- Added support for plugin for those without FFMPEG installed on their server.  Without FFMPEG installed, this plugin doesn't take advantage of all the features it offers.
+- Added default splash image in case this plugin is used without FFMPEG.
+
+= 1.0 =
+Initial release
+
+== Arbitrary section ==
+
+== A brief Markdown Example ==
+
---
+++ wpvp-widgets.php
@@ -0,0 +1,144 @@
+<?php 
+//extend widget class
+class WPVideosForPostsWidget extends WP_Widget
+{
+        function WPVideosForPostsWidget(){
+                $widget_ops = array(    'classname' => 'WP Videos Posts',
+                                        'description' => 'Displays the embedded videos from YouTube or Vimeo');
+                $this->WP_Widget('WPVideosForPostsWidget','WP Videos Posts',$widget_ops);
+        }
+        function form($instance){
+                $instance = wp_parse_args((array) $instance, array('title'=> '','width'=>'','cat_checkbox'=>'','num_posts'=>'','display'=>'','display_type'=>'','post_title'=>'','author'=>'','excerpt'=>''));
+                $title = $instance['title'];
+		$width = $instance['width'];
+		$height = $instance['height'];
+		$categories = $instance['cat_checkbox'];
+		$num_posts = $instance['num_posts'];
+		$display = $instance['display'];
+		$display_type = $instance['display_type'];
+		$post_title = $instance['post_title'];
+		$author = $instance['author'];
+		$excerpt = $instance['excerpt'];
+		?>
+                <p>
+                        <label for="<?php echo $this->get_field_id('title');?>">
+                        <?php _e('Title:');?>
+                        </label>
+			<?php 
+		//	print_r($instance);
+			?>
+                        <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" />
+			<p>
+			<label for="<?php echo $this->get_field_id('width');?>">
+			<?php _e('Width of a video item (px):');?>
+			</label>
+                        <input style="width:60px;" width="40" id="<?php echo $this->get_field_id('width'); ?>" name="<?php echo $this->get_field_name('width'); ?>" type="text" value="<?php echo attribute_escape($width); ?>" />
+			</p>
+			<p>
+			<label for="<?php echo $this->get_field_id('height');?>">
+                        <?php _e('Height of a video item (px):');?>
+                        </label>
+                        <input style="width:60px;" id="<?php echo $this->get_field_id('height'); ?>" name="<?php echo $this->get_field_name('height'); ?>" type="text" value="<?php echo attribute_escape($height); ?>" />
+			</p>
+			<p>
+			<label for="<?php echo $this->get_field_id('num_posts');?>">
+                        <?php _e('Number of posts:');?>
+                        </label>
+			<input style="width:60px;" id="<?php echo $this->get_field_id('num_posts'); ?>" name="<?php echo $this->get_field_name('num_posts'); ?>" type="text" value="<?php echo attribute_escape($num_posts); ?>" />
+			</p>	
+			<p>
+			<label for="<?php echo $this->get_field_id('display_type');?>">
+                        <b><?php _e('Display Options:')?></b>
+                        </label>
+			</p>
+			<p>
+			<label for="<?php echo $this->get_field_id('display');?>">
+                        <?php _e('Layout:');?>
+			</label>
+			<input type="radio" name="<?php echo $this->get_field_name('display');?>" value="v" <?php if($instance['display']=='v'){ echo 'checked="checked"'; }?>/><?php _e('Vertical');?>
+			<input type="radio" name="<?php echo $this->get_field_name('display');?>" value="h" <?php if($instance['display']=='h'){ echo 'checked="checked"'; }?>/><?php _e('Horizontal');?>
+			</p>
+			<p>
+			<label><?php _e('Display type:');?></label>
+                        <input type="radio" name="<?php echo $this->get_field_name('display_type');?>" value="p" <?php if($instance['display_type']=='p'){ echo 'checked="checked"'; }?>/><?php _e('Player');?>
+                        <input type="radio" name="<?php echo $this->get_field_name('display_type');?>" value="th" <?php if($instance['display_type']=='th'){ echo 'checked="checked"'; }?>/><?php _e('Thumbnails');?>
+			</p>
+			<p>
+                        <input type="checkbox" name="<?php echo $this->get_field_name('post_title');?>" value="post_title" <?php if($instance['post_title']=='post_title'){ echo 'checked="checked"';};?>/> <?php _e('Display Post Title');?>
+                        </p>
+			<p>
+			<input type="checkbox" name="<?php echo $this->get_field_name('author');?>" value="author" <?php if($instance['author']=='author'){ echo 'checked="checked"';};?>/> <?php _e('Display Author');?>
+			</p>
+			<p>
+                        <input type="checkbox" name="<?php echo $this->get_field_name('excerpt');?>" value="excerpt" <?php if($instance['excerpt']=='excerpt'){ echo 'checked="checked"';};?>/> <?php _e('Display Excerpt');?>
+                        </p>
+			<p>
+                        <strong><?php _e("Categories to display from: " ); ?></strong><br />
+			<div style="height:145px;overflow:auto;">
+                        <ul style="list-style-type:none;">
+                        <?php
+				$args = array('hide_empty'=>0);
+				$categories = get_categories($args);
+				foreach($categories as $category){
+					$options .= '<li><input type="checkbox" id="'.$this->get_field_id('cat_checkbox').'[]" name="'.$this->get_field_name('cat_checkbox').'[]"';
+					if(is_array($instance['cat_checkbox'])){
+						foreach($instance['cat_checkbox'] as $cats){
+							if($cats == $category->term_id){
+								$options .= ' checked = "checked"';
+							}
+						}
+					}
+					$options .= ' value="'.$category->term_id.'" /> ';
+					$options .= $category->cat_name.'</li>';
+				}
+				echo $options;
+                        ?>
+                        </ul>
+			</div>
+                	</p>
+                </p>
+<?php
+        }
+        function update($new_instance, $old_instace){
+                $instance = $old_instance;
+                $instance['title'] = $new_instance['title'];
+		$instance['width'] = $new_instance['width'];
+		$instance['height'] = $new_instance['height'];
+		$instance['cat_checkbox'] = $new_instance['cat_checkbox'];
+		$instance['num_posts'] = $new_instance['num_posts'];
+		$instance['display'] = $new_instance['display'];
+		$instance['display_type'] = $new_instance['display_type'];
+		$instance['post_title']= $new_instance['post_title'];
+		$instance['author']=$new_instance['author'];
+		$instance['excerpt']=$new_instance['excerpt'];
+                return $instance;
+        }
+        function widget($args, $instance){
+                extract($args, EXTR_SKIP);
+                echo $before_widget;
+                $title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
+		$width = empty($instance['width']) ? ' ' : apply_filters('widget_title', $instance['width']);
+		$height = empty($instance['height']) ? ' ' : apply_filters('widget_title', $instance['height']);
+		$categories = empty($instance['post_category']) ? ' ' : apply_filters('widget_title', $instance['post_category']);
+		$num_posts = empty($instance['num_posts']) ? ' ' : apply_filters('widget_title', $instance['num_posts']);
+		$display = empty($instance['display']) ? ' ' : apply_filters('widget_title', $instance['display']);
+		$display_type = empty($instance['display_type']) ? ' ' : apply_filters('widget_title', $instance['display_type']);
+		$display_type = empty($instance['post_title']) ? ' ' : apply_filters('widget_title', $instance['post_title']);
+		$display_type = empty($instance['author']) ?  ' ' : apply_filters('widget_title', $instance['author']);
+		$display_type = empty($instance['excerpt']) ?  ' ' : apply_filters('widget_title', $instance['excerpt']);
+                if(!empty($title))
+                        echo $before_title .$title . $after_title;
+
+        //widget code
+                echo wpvp_widget_function($instance);
+                echo $after_widget;
+        }
+}
+//add_action('widgets_init', create_function('','return register_widget("WPVideosForPostsWidget");'));
+
+function wpvp_widget_function($instance){
+        require_once('wpvp-functions.php');
+        $video_posts = wpvp_widget_latest_posts($instance);
+        return $video_posts;
+}
+?>
