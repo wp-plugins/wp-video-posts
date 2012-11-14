fce95767fed702671d128404ff10776d3d2b3a60
cmstactics <cmstactics@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-14 22:25:45 +0000

    Adding previous versions to the repository
    git-svn-id: https://plugins.svn.wordpress.org/wp-video-posts/tags/1.3@625419 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wpvp-options.php
@@ -0,0 +1,127 @@
+<?php 
+if (ffmpegCommandExists("ffmpeg")>0) {
+	// FFMPEG Exists on server
+        //echo "FFMPEG IS installed";
+	$ffmpeg_installed = true;
+} else {
+	// No FFMPEG
+        //echo "FFMPEG is NOT installed";
+	$ffmpeg_installed = false;
+}
+
+function ffmpegCommandExists($command) {
+    $command = escapeshellarg($command);
+    $exists = exec($command." -h",$out);
+    return sizeof($out);
+}
+
+
+if($_POST['wpvp_hidden'] == 'Y') {
+        //Form data sent
+        $wpvp_width = $_POST['wpvp_video_width'];
+	$wpvp_height = $_POST['wpvp_video_height'];
+        $wpvp_thumb_width = $_POST['wpvp_thumb_width'];
+	$wpvp_thumb_height = $_POST['wpvp_thumb_height'];
+	$wpvp_capture_image = $_POST['wpvp_capture_image'];
+//	$wpvp_category_list = $_POST['post_category'];
+	
+	update_option('wpvp_video_width', $wpvp_width);
+	update_option('wpvp_video_height', $wpvp_height);
+	update_option('wpvp_thumb_width', $wpvp_thumb_width);
+	update_option('wpvp_thumb_height', $wpvp_thumb_height);
+	update_option('wpvp_capture_image', $wpvp_capture_image);
+//	update_option('post_category', $wpvp_category_list);
+?>
+<div class="updated"><p><strong><?php _e('Options saved.' ); ?></strong></p></div>
+<?php
+} else{
+	$wpvp_width = get_option('wpvp_video_width');
+	$wpvp_height = get_option('wpvp_video_height');
+	$wpvp_thumb_width = get_option('wpvp_thumb_width');
+	$wpvp_thumb_height = get_option('wpvp_thumb_height');
+	$wpvp_capture_image = get_option('wpvp_capture_image');
+//	$wpvp_category_list = get_option('post_category');
+}
+?>
+<div class="wrap">
+	<?php	echo "<h2>" . __( 'WP Video Posts Options' ) . "</h2>";?>
+
+	<!-- PayPal Donate -->
+	<?php echo "<h3>Please donate if you enjoy this plugin (WPVP):</h3>"; ?>
+		<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+			<input type="hidden" name="cmd" value="_s-xclick">
+			<input type="hidden" name="hosted_button_id" value="J535UTFPCXFQC">
+			<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+			<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+		</form>
+	<hr>
+
+	<?php 	if(!$ffmpeg_installed){
+		echo '<h3 style="color: red;">FFMPEG is not installed on the server, therefore this plugin cannot function properly.<br />Please verify with your administrator or hosting provider to have this installed and configured.</h3><br />';
+	} ?>	
+	<form name="wpvp_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+		<input type="hidden" name="wpvp_hidden" value="Y">
+		<p>
+                        <strong><?php _e("Converted video width: " ); ?></strong>
+			<input type="text" name="wpvp_video_width" value="<?php echo $wpvp_width; ?>" size="5" /> <?php _e("(in pixels)"); ?>
+		</p>
+		<p>
+                        <strong><?php _e("Converted video height: " ); ?></strong>
+                        <input type="text" name="wpvp_video_height" value="<?php echo $wpvp_height; ?>" size="5" /> <?php _e("(in pixels)"); ?> 
+                </p>
+		<p>
+                        <strong><?php _e("Converted video thumbnail width: " ); ?></strong>
+                        <input type="text" name="wpvp_thumb_width" value="<?php echo $wpvp_thumb_width; ?>" size="5" /> <?php _e("(in pixels)"); ?> 
+                </p>
+		<p>
+                        <strong><?php _e("Converted video thumbnail height: " ); ?></strong>
+                        <input type="text" name="wpvp_thumb_height" value="<?php echo $wpvp_thumb_height; ?>" size="5" /> <?php _e("(in pixels)"); ?>
+                </p>
+		<p>
+                        <strong><?php _e("Capture Splash Image: " ); ?></strong>
+                        <input type="text" name="wpvp_capture_image" value="<?php echo $wpvp_capture_image; ?>" size="5" /> <?php _e("(in seconds)"); ?>
+                </p>
+<?php /*You can uncomment it out to let the user choose categories to assign video posts to by default. It will be used in case the automated functionality is desired. Some changes have to be done to the code in wpvp-functions.php*/
+/*
+		<p>
+                        <style>
+                                ul.children{
+                                        padding-left:15px;
+                                }
+                        </style>
+                        <strong><?php _e("Choose Category(ies) to assign video posts to: " ); ?></strong><br />
+                        <ul style="list-style-type:none;">
+                        <?php
+                                wp_category_checklist('','',$wpvp_category_list,'','','');
+                        ?>
+                        </ul>
+                </p>
+*/?>
+		<p>
+			<strong><?php _e("Allowed video extensions for uploading");?></strong>
+			<ul>
+			<?php 
+				$allowed = get_allowed_mime_types();
+				foreach($allowed as $key=>$value){
+					$t = explode('/',$value);
+					if($t[0]=='video'){
+						$types .= '<li>-'.$key.' ('.$value.')';
+					}
+				}	
+				echo $types;
+			?>
+			</ul>
+		</p>
+		<p class="submit">
+        	        <input type="submit" name="Submit" value="<?php _e('Update Options' ) ?>" />
+	        </p>
+        </form>
+	<br />
+	<hr>
+	<div style="font-weight: bold; font-size: 14px;">Youtube and Vimeo Embed Instructions</div>
+	WP Video Posts allows the embed of Youtube and Vimeo videos with the use of the following shortcodes:
+	<br /><br />
+	<strong>Youtube:</strong> [wpvp_embed type=youtube video_code=vAFQIciWsF4 width=560 height=315]
+	<br /><br />
+	<strong>Vimeo:</strong> [wpvp_embed type=vimeo video_code=23117398 width=500 height=281]
+</div>
---
+++ wpvp.php
@@ -0,0 +1,199 @@
+<?php
+/*
+Plugin Name: WP Video Posts
+Plugin URI: http://cmstactics.com
+Description: WP Video Posts creates a custom post for uploaded videos. You can upload videos of different formats (FLV, F4V, MP4, AVI, MOV, 3GP and WMV) and the plugin will convert it to MP4 and play it using Flowplayer.  
+Version: 1.3
+Author: Alex Rayan, cmstactics 
+Author URI: http://cmstactics.com
+License: GPLv2 or later
+*/
+
+//add_action('widget_init','wpvp_register_widgets');
+require_once( dirname(__FILE__) . '/wpvp-widgets.php');
+add_action('widgets_init', create_function('', 'register_widget("WPVideosForPostsWidget");'));
+
+add_action('init', 'wpvp_init');
+function wpvp_init(){
+	wpvp_register();
+        register_taxonomy_for_object_type('category','videos');
+	wpvp_add_menu_options();		
+}
+
+function wpvp_add_menu_options(){
+	if(is_admin()){
+		function wpvp_options_page(){
+        		add_options_page('WP Video Posts','WP Video Posts','manage_options','wp-video-posts','wpvp_options');
+        	}
+        	add_action('admin_menu','wpvp_options_page');
+
+        	function wpvp_options(){
+                	include('wpvp-options.php');
+        	}
+	}
+}
+
+function wpvp_head_includes(){
+	wp_register_script('wpvp_flowplayer', plugins_url('/js/', __FILE__).'flowplayer-3.2.10.min.js');
+        wp_enqueue_script('wpvp_flowplayer');
+}
+add_action('wp_head','wpvp_head_includes');
+
+function wpvp_footer_includes(){
+	echo '<script>flowplayer("a.myPlayer", "'.plugins_url('/js/', __FILE__).'flowplayer-3.2.11.swf", { clip:{ autoPlay:false, autoBuffering:true }, plugins: { controls: { volume: true } }});</script>';
+}
+add_action('wp_footer','wpvp_footer_includes');
+
+//register custom post type
+function wpvp_register(){
+        $labels = array(
+                'name' => _x('Videos', 'post type general name'),
+                'singular_name' => _x('Video Item', 'post type singular name'),
+                'add_new' => _x('Add New Video', 'video item'),
+                'add_new_item' => __('Add New Video Item'),
+                'edit_item' => __('Edit Video Item'),
+                'new_item' => __('New Video Item'),
+                'view_item' => __('View Video Item'),
+                'search_items' => __('Search Video'),
+                'not_found' =>  __('Nothing found'),
+                'not_found_in_trash' => __('Nothing found in Trash'),
+                'parent_item_colon' => ''
+        );
+
+        $args = array(
+                'labels' => $labels,
+                'public' => true,
+                'publicly_queryable' => true,
+                'show_ui' => true,
+                'query_var' => true,
+                'menu_icon' => plugins_url('/images/', __FILE__) . 'videos_menu_icon.png',
+                //'rewrite' => true,
+                'rewrite' => array('slug'=>'videos'),
+                'capability_type' => 'post',
+                'hierarchical' => false,
+                'menu_position' => null,
+                'supports' => array('title','editor','thumbnail')
+          );
+
+        register_post_type( 'videos' , $args );
+	
+}
+
+function wpvp_encode($ID){
+	require_once('wpvp-functions.php');
+	$postID = intval($_REQUEST['post_id']);
+	if($postID){
+		$encode_video = encode($ID);
+		return $encode_video;
+	}
+	else{
+		return;
+	}
+}
+add_action('add_attachment', 'wpvp_encode');
+
+function wpvp_save_attachment($data){
+	require_once( dirname(__FILE__) . '/wpvp-functions.php');
+	$save_attachment = attachment_save($data);
+	return $save_attachment;
+}
+add_filter('attachment_fields_to_save', 'wpvp_save_attachment');
+
+function wpvp_edit_attachment($data){
+	require_once( dirname(__FILE__) . '/wpvp-functions.php');
+	$edit_attachment = attachment_edit($data);
+	return $edit_attachment;
+}
+add_filter('attachment_fields_to_edit',  'wpvp_edit_attachment');
+
+//register shortcode for flowplayer videos
+function wpvp_register_shortcode($atts){
+	extract(shortcode_atts(array(
+		'src'=>'',
+		'width'=>'640',
+		'height'=>'360',
+		'splash'=>''
+	),$atts));
+	$flowplayer_code = '<a href="'.$src.'" class="myPlayer" style="display:block;width:'.$width.'px;height:'.$height.'px;margin:10px auto"><img width="'.$width.'" height="'.$height.'" src="'.$splash.'" alt="" /></a>';
+	return $flowplayer_code;
+}
+add_shortcode('wpvp_flowplayer','wpvp_register_shortcode');
+
+//register shortcode to embed videos via video codes
+function wpvp_register_embed_shortcode($atts){
+	require_once( dirname(__FILE__) . '/wpvp-functions.php');
+	extract(shortcode_atts(array(
+		'video_code'=>'',
+		'width'=>'560',
+		'height'=>'315',
+		'type'=>''
+	),$atts));
+	$embedCode = wpvp_video_embed($video_code, $width, $height, $type); 
+	return $embedCode;
+}
+add_shortcode('wpvp_embed','wpvp_register_embed_shortcode');
+
+//insert the shortcode into the post content
+function wpvp_insert_shortcode_into_post ($html, $id, $attachment) {
+        $postID = intval($_REQUEST['post_id']);
+        $postObj = get_post($postID);
+	$postContent = $html;
+        if($postObj->post_type=='videos'){
+		require_once( dirname(__FILE__) . '/wpvp-functions.php');	
+		$postContent = wpvp_insert_video_into_post($postContent, $id, $attachment);
+	} 
+	return $postContent;
+}
+add_filter('media_send_to_editor','wpvp_insert_shortcode_into_post',20,3);
+
+//add support for videos of defined extensions
+function wpvp_add_video_formats_support($existing_mimes){
+	require_once( dirname(__FILE__) . '/wpvp-functions.php');
+	$formatsSupported = wpvp_video_upload_mime_types($existing_mimes);
+	return $formatsSupported;
+}
+add_filter('upload_mimes','wpvp_add_video_formats_support');
+
+function wpvp_video_code_add_meta($id){
+	global $post;
+	$post_content = $_POST['post_content'];
+	if(!wp_is_post_revision($id)){
+        	$post_id = $id;
+		$post_content = $_POST['post_content'];
+     		if($_POST['post_type']!= 'videos'){
+			//do nothing, not our post type
+        	}
+		else {
+        		//if( (preg_match('/youtube/', $post_content)) || (preg_match('/vimeo/', $post_content)) ){
+			if(preg_match('/wpvp_embed/',$post_content)){
+                		$video_code_start = strpos($post_content, 'video_code=');
+                		$video_code = substr($post_content, $video_code_start+11);
+                		$video_code_end = strpos($video_code, ' ');
+                		$video_code = substr($video_code, 0, $video_code_end);
+         	       		update_post_meta($post_id, 'wpvp_video_code',$video_code);
+        		} else if(preg_match('/wpvp_flowplayer/',$post_content)){
+				//do nothing - no code found
+				$video_code_start = strpos($post_content, 'src=');
+				$splash_code_start = strpos($post_content, 'splash=');
+				$video_code = substr($post_content, $video_code_start+4);
+				$splash_code = substr($post_content, $splash_code_start+7);
+				$video_code_end = strpos($video_code,' ');
+				$video_code = substr($video_code, 0, $video_code_end);
+				$splash_code_end = strpos($splash_code,']');
+				$splash_code = substr($splash_code, 0, $splash_code_end);
+				$fl_codes = array('src'=>$video_code,'splash'=>$splash_code);
+				$fl_codes = json_encode($fl_codes);
+				update_post_meta($post_id, 'wpvp_fp_code',$fl_codes);
+			}
+		}
+	}
+	else {
+		//do nothing, this is a revision, not an actual post
+	}
+}
+add_action('publish_videos','wpvp_video_code_add_meta',20);
+//add_action('pre_post_update','wpvp_video_code_add_meta');
+//add_action('new_to_publish_videos', 'wpvp_video_code_add_meta');
+//add_action('draft_to_publish_videos', 'wpvp_video_code_add_meta');
+//add_action('pending_to_publish_videos', 'wpvp_video_code_add_meta');
+?>
